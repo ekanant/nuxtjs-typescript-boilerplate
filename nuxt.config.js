d408ffabfab5ae6734d7f97518fc3be02108d933
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-07-07 17:25:08 +0700

    :wrench: fix wrong srcDir

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  srcDir: 'src/',
+  srcDir: './src/',
   mode: 'universal',
   /*
    ** Headers of the page
@@ -12,10 +12,10 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
-      },
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
    ** Customize the progress-bar color
@@ -35,7 +35,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxt/typescript-build',
+    '@nuxt/typescript-build'
   ],
   /*
    ** Nuxt.js modules
@@ -48,6 +48,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {},
-  },
+    extend (config, ctx) {}
+  }
 }

bcd4db23841a3f25403ef75e03005f8df5b2223c
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-07-08 08:37:02 +0700

    create getTodos service

---
+++ src/api/todo.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import axios, { AxiosResponse } from 'axios'
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export const getTodos = async ():Promise<Todo[]> => {
+  return await axios.get('https://jsonplaceholder.typicode.com/todos').then((response: AxiosResponse<Todo[]>) => {
+    const { data } = response
+    return data
+  })
+}
